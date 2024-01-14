e1dcb912c79b23645508a1f7bec26635fa6d56d0
NakerTheFirst <kcholysk@gmail.com>
2024-01-14 11:37:30 +0100

    Refactor Car class into CarUtils class

--- main.py
+++ main.py
@@ -1,57 +1,43 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
-class Car:
-    def __init__(self, mpg: float, cylinders: int, displacement: float, horsepower: float, weight: float, accel: float,
-                 model_year: int, origin: int, name: str):
-        self.__mpg = mpg
-        self.__cylinders = cylinders
-        self.__displacement = displacement
-        self.__horsepower = horsepower
-        self.__weight = weight
-        self.__accel = accel
-        self.__model_year = model_year
-        self.__origin = origin
-        self.__name = name
-
-    def __str__(self):
-        return (f"mpg: {self.__mpg}, cylinders: {self.__cylinders}, displacement: {self.__displacement}, horsepower: "
-                f"{self.__horsepower}, weight: {self.__weight}, accel: {self.__accel}, model year: {self.__model_year} "
-                f"origin: {self.__origin}, name: {self.__name}")
-
-
-def read_cars_from_file(filename):
-    cars = []
-    with open(filename, 'r') as file:
-        for line in file:
-            parts = line.split()
-
-            # Extracting numeric values
-            try:
-                mpg, cylinders, displacement, horsepower, weight, accel, model_year, origin = map(float, parts[:8])
-            except ValueError:
-                continue
-
-            model_year, origin = int(model_year), int(origin)
-
-            # Extracting car name (last part)
-            name = ' '.join(parts[8:]).replace('"', '')  # Removing quotes
-
-            # Creating Car instance
-            car = Car(mpg, cylinders, displacement, horsepower, weight, accel, model_year, origin, name)
-            cars.append(car)
-    return cars
+class CarUtils:
+    @staticmethod
+    def read_cars_from_file(filename) -> pd.DataFrame:
+        car_list = []
+        with open(filename, 'r') as file:
+            for line in file:
+                parts = line.split()
+                try:
+                    mpg, cylinders, displacement, horsepower, weight, accel, model_year, origin = map(float, parts[:8])
+                except ValueError:
+                    continue
+                model_year, origin = int(model_year), int(origin)
+                name = ' '.join(parts[8:]).replace('"', '')
+                car_dict = {
+                    'mpg': mpg,
+                    'cylinders': cylinders,
+                    'displacement': displacement,
+                    'horsepower': horsepower,
+                    'weight': weight,
+                    'accel': accel,
+                    'model_year': model_year,
+                    'origin': origin,
+                    'name': name
+                }
+                car_list.append(car_dict)
+        return pd.DataFrame(car_list)
 
 
 def main():
 
     # Read the data
     file_path = 'auto-mpg.data'
-    cars = read_cars_from_file(file_path)
+    df_cars = CarUtils.read_cars_from_file(file_path)
 
-    for x in cars:
-        print(x)
+    print(df_cars.to_string())
 
     return 0
 
