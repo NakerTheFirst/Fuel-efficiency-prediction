e82cf75556fd6d4b4da2a9c74479a0be9170e4dc
NakerTheFirst <kcholysk@gmail.com>
2024-01-14 15:27:52 +0100

    Add training data standarisation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
 
 
-class CarUtils:
+class CarsUtils:
     @staticmethod
     def read_cars_from_file(filename) -> pd.DataFrame:
         car_list = []
@@ -30,12 +32,28 @@ class CarUtils:
                 car_list.append(car_dict)
         return pd.DataFrame(car_list)
 
+    @staticmethod
+    def standardise_data(df) -> pd.DataFrame:
+        scaler = preprocessing.StandardScaler()
+
+        # Select columns to standardise
+        numerical_cols = ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'accel', 'model_year', 'origin']
+
+        df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
+        return df
+
 
 def main():
 
     # Read the data
     file_path = 'auto-mpg.data'
-    df_cars = CarUtils.read_cars_from_file(file_path)
+    df_cars = CarsUtils.read_cars_from_file(file_path)
+
+    # Split the data
+    train_data, test_data = train_test_split(df_cars, test_size=0.20, random_state=42)
+
+    # Standardise the training dataset
+    train_data_standardised = CarsUtils.standardise_data(train_data)
 
     print(df_cars.to_string())
 
