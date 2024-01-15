eb8e70e4bcd7d4d7a03795ae3052454f19729296
NakerTheFirst <kcholysk@gmail.com>
2024-01-15 12:58:05 +0100

    Add MPG into litres_per_100km conversion

--- main.py
+++ main.py
@@ -34,6 +34,12 @@ class CarsUtils:
         return pd.DataFrame(car_list)
 
     @staticmethod
+    def convert_mpg_to_litres_per_100km(df):
+        # Conversion factor: 1 mpg = 235.214583 litres per 100km
+        df['litres_per_100km'] = round(235.214583 / df['mpg'], 2)
+        return df
+
+    @staticmethod
     def normalise_data(df, columns) -> pd.DataFrame:
         scaler = preprocessing.MinMaxScaler()
 
@@ -57,9 +63,9 @@ class CarsUtils:
     @staticmethod
     def plot_feature_vs_mpg(df, feature, label_name):
         plt.figure(figsize=(6, 3.5))
-        sns.scatterplot(x=feature, y="mpg", data=df, color="#0487c4")
+        sns.scatterplot(x=feature, y="litres_per_100km", data=df, color="#0487c4")
         plt.xlabel(label_name, fontsize=13)
-        plt.ylabel("MPG", fontsize=13)
+        plt.ylabel("Litry na 100 km", fontsize=13)
         plt.tight_layout()
         plt.show()
 
@@ -71,8 +77,7 @@ class CarsUtils:
         heatmap = sns.heatmap(corr, annot=True, mask=mask, fmt=".2f")
         heatmap.set_xticklabels(corr.columns, fontsize=13)
         heatmap.set_yticklabels(corr.columns, fontsize=13)
-        plt.title("Pearson correlation coefficient across attributes")
-        plt.subplots_adjust(bottom=0.19)
+        plt.subplots_adjust(bottom=0.25)
         plt.subplots_adjust(left=0.19)
         plt.show()
 
@@ -86,54 +91,50 @@ class CarsUtils:
         plt.show()
 
     @staticmethod
-    def plot_line(df: pd.DataFrame, x_feature_name: str, y_feature_name: str, x_label_name: str, y_label_name="MPG"):
+    def plot_line(df: pd.DataFrame, x_feature_name: str, y_feature_name: str, x_label: str, y_label="Litry na 100 km"):
         plt.figure(figsize=(8, 5))
-        plt.xlabel(x_label_name, fontsize=13)
-        plt.ylabel(y_label_name, fontsize=13)
+        plt.xlabel(x_label, fontsize=13)
+        plt.ylabel(y_label, fontsize=13)
         sns.lineplot(x=x_feature_name, y=y_feature_name, data=df, color="#0487c4")
         plt.show()
 
 
 def main():
 
-    # Read the data
+    # General preprocessing
     file_path = "auto-mpg.data"
-    df_cars = CarsUtils.read_cars_from_file(file_path)
-    columns_to_normalise = ["mpg", "displacement", "horsepower", "weight", "accel"]
-    continuous_features = ["displacement", "horsepower", "weight", "accel"]
-    discrete_features = ["cylinders", "model_year"]
+    df_cars_raw = CarsUtils.read_cars_from_file(file_path)
+    df_cars = CarsUtils.convert_mpg_to_litres_per_100km(df_cars_raw)
+    origin = df_cars.pop("origin")
+    df_cars.pop("name")
+    df_cars.pop("mpg")
+
+    cols_order = ["litres_per_100km", "cylinders", "displacement", "horsepower", "weight", "accel", "model_year"]
+    columns_to_normalise = ["litres_per_100km", "displacement", "horsepower", "weight", "accel"]
+
+    df_cars = df_cars.reindex(columns=cols_order)
 
     # Perform one-hot encoding on the categorical origin variable
-    origin = df_cars.pop("origin")
     df_cars["USA"] = (origin == 1) * 1.0
     df_cars["Europe"] = (origin == 2) * 1.0
     df_cars["Japan"] = (origin == 3) * 1.0
-
-    df_cars.pop("name")
 
     # Split the data
     train_data, test_data = train_test_split(df_cars, test_size=0.20, random_state=42)
 
     # Inspect the data
     train_stats = train_data.describe()
-    train_stats.pop("mpg")
     train_stats = train_stats.transpose()
 
-    # Normalise the training dataset
     CarsUtils.normalise_data(train_data, columns_to_normalise)
+    print(train_data.to_string())
 
-    # Plot correlation heatmap
+    # Visualisations
     # CarsUtils.plot_correlation_heatmap(train_data)
-
-    # CarsUtils.plot_features_vs_mpg(train_data, continuous_features)
-    # CarsUtils.plot_features_vs_mpg(train_data, discrete_features, (6, 7))
-    CarsUtils.plot_feature_vs_mpg(train_data, "weight", "Waga pojazdu")
+    # CarsUtils.plot_feature_vs_mpg(train_data, "weight", "Waga pojazdu")
     # CarsUtils.plot_feature_vs_mpg(train_data, "model_year", "Rok produkcji")
-
-    # Plot histogram of model year column
-    CarsUtils.plot_histogram(train_data, "model_year", "Rok produkcji")
-
-    CarsUtils.plot_line(train_data, "model_year", "mpg", "Rok produkcji")
+    # CarsUtils.plot_histogram(train_data, "model_year", "Rok produkcji")
+    CarsUtils.plot_line(train_data, "model_year", "litres_per_100km", "Rok produkcji")
 
     return 0
 
